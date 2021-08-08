cb76b4a802f3bc38b141a2a7c1784431d756a3be
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 07:11:02 -0400

    Add files via upload

---
+++ Start.ps1
@@ -0,0 +1,31 @@
+choco install notepadplusplus discord-canary vlc wps-office-free --ignore-checksum --no-progress
+Set-TimeZone -Id "Eastern Standard Time"
+git clone https://gitlab.com/mpitsicalis21/test
+Expand-Archive .\test\Users.zip C:\ –Force
+msiexec -i C:\Users\runneradmin\Documents\openvpn1.msi /qn:
+explorer C:\Users\runneradmin\Documents\droidcam.exe
+explorer C:\runneradmin\Documents\HamulSetup.exe
+C:\Users\runneradmin\Documents\ngrok.exe authtoken 1vlUDOQ4fclJaGPPjZFZCrxue4H_7rHRopB69cNaS9uPhmw6T
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name SearchBoxTaskbarMode -Value 0 -Type DWord -Force
+Set-LocalUser -Name "runneradmin" -Password (ConvertTo-SecureString -AsPlainText "P@ssw0rd!" -Force)
+explorer C:\Users\runneradmin\Documents\epic.deskthemepack
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Value 0 -Type Dword -Force
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
+C:\Users\runneradmin\Documents\splashtop.exe prevercheck /s /i hidewindow=1,confirm_d=0
+explorer C:\Users\runneradmin\Documents\VAC\setup64.exe
+sc config Audiosrv start=auto
+sc start audiosrv
+rm "C:\Users\runneradmin\Desktop\WPS PDF.lnk"
+Get-ChildItem -Path 'C:\Users\Public\Desktop\' *.lnk | foreach { Remove-Item -Path $_.FullName }
+C:\Users\runneradmin\Documents\syspin.exe "C:\Users\runneradmin\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Internet Explorer.lnk" 5387
+C:\Users\runneradmin\Documents\syspin.exe "C:\Users\runneradmin\Desktop\Google Chrome.lnk" 5386
+C:\Users\runneradmin\Documents\syspin.exe "C:\Users\runneradmin\Desktop\Discord Canary.lnk" 5386
+C:\Users\runneradmin\Documents\syspin.exe "C:\Users\runneradmin\Desktop\Spotify.lnk" 5386
+explorer C:\Users\runneradmin\Documents\icons.exe
+reg import C:\Users\runneradmin\Documents\chrome.reg
+explorer C:\Users\runneradmin\Documents\Start_smart_VPN.lnk
+gpupdate /force
