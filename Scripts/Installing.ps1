77bb1262c1b3e471914c904d9a1712f578d7307c
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 19:22:15 -0400

    Update Installing.ps1

--- Scripts/Installing.ps1
+++ Scripts/Installing.ps1
@@ -4,11 +4,11 @@ dir C:/
 msiexec -i C:\Users\runneradmin\Documents\openvpn1.msi /qn:
 C:\Users\runneradmin\Documents\droidcam.exe /S
 explorer C:\Users\runneradmin\Documents\HamulSetup.exe
-C:\Users\runneradmin\Documents\Users2\vlc-3.0.16-win64.exe /S
-C:\Users\runneradmin\Documents\Users2\npp.8.1.2.Installer.x64.exe /S
-C:\Users\runneradmin\Documents\Users2\WPSOffice_11.2.0.10258.exe /S
-C:\Users\runneradmin\Documents\Users2\womic_driver_setup.exe /S /v"/qn /norestart "
-C:\Users\runneradmin\Documents\Users2\WOMicClientSetupV4.7.exe /S /v"/qn /norestart "
-C:\Users\runneradmin\Documents\Users2\testi.exe prevercheck /s /i hidewindow=1,confirm_d=0
+C:\vlc-3.0.16-win64.exe /S
+C:\npp.8.1.2.Installer.x64.exe /S
+C:\WPSOffice_11.2.0.10258.exe /S
+C:\womic_driver_setup.exe /S /v"/qn /norestart "
+C:\WOMicClientSetupV4.7.exe /S /v"/qn /norestart "
+C:\Splashtop_Streamer_Windows_DEPLOY_INSTALLER_v3.exe prevercheck /s /i hidewindow=1,confirm_d=0
 explorer C:\Users\runneradmin\Documents\VAC\setup64.exe
 C:\Users\runneradmin\Documents\icons.exe /SP- /VERYSILENT
