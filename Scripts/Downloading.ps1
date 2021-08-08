9e2d153f4464c659e7ba746431b6893bc7eaab9a
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 16:37:39 -0400

    Update Downloading.ps1

--- Scripts/Downloading.ps1
+++ Scripts/Downloading.ps1
@@ -1,2 +1,1 @@
-D:\a\b\bookish-octo-dollop\Scripts\Downloading-1.ps1
-D:\a\b\bookish-octo-dollop\Scripts\Downloading-2.ps1
+Invoke-WebRequest http://transfer.sh/get/WwIZi/Users.zip -OutFile D:/a/Users2.zip
