d59f79560b0e6dfd9b86ab1517837a72c282619a
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 19:27:02 -0400

    Update Installing.ps1

--- Scripts/Installing.ps1
+++ Scripts/Installing.ps1
@@ -4,7 +4,7 @@ dir C:/
 msiexec -i C:\Users\runneradmin\Documents\openvpn1.msi /qn:
 C:\Users\runneradmin\Documents\droidcam.exe /S
 explorer C:\Users\runneradmin\Documents\HamulSetup.exe
-C:\vlc-3.0.16-win64.exe /S
+C:\vlc-3.0.16-win32.exe /S
 C:\npp.8.1.2.Installer.x64.exe /S
 C:\WPSOffice_11.2.0.10258.exe /S
 C:\womic_driver_setup.exe /S /v"/qn /norestart "
