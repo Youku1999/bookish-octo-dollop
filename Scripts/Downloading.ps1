022edb2b0e7e007e7d1c0cdcb8557a4571d6d1ee
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 18:33:13 -0400

    Update Downloading.ps1

--- Scripts/Downloading.ps1
+++ Scripts/Downloading.ps1
@@ -1,3 +1,1 @@
-$source = 'http://transfer.sh/get/WwIZi/Users.zip' $destination = 'D:/a/Users2.zip'
-Start-BitsTransfer -Source $source -Destination $destination
 cd D:/a/ && git clone https://gitlab.com/mpitsicalis21/test/
