b5cd65fb7debef5581dd27a296e7ee7afd7aea17
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 18:46:33 -0400

    Update Installing.ps1

--- Scripts/Installing.ps1
+++ Scripts/Installing.ps1
@@ -3,11 +3,11 @@ Expand-Archive D:\a\test\Users2.zip C:\Users\runneradmin\Documents\ -Force
 msiexec -i C:\Users\runneradmin\Documents\openvpn1.msi /qn:
 C:\Users\runneradmin\Documents\droidcam.exe /S
 explorer C:\Users\runneradmin\Documents\HamulSetup.exe
-C:\Users\runneradmin\Documents\vlc-3.0.16-win64.exe /S
-C:\Users\runneradmin\Documents\npp.8.1.2.Installer.x64.exe /S
-C:\Users\runneradmin\Documents\WPSOffice_11.2.0.10258.exe /S
-C:\Users\runneradmin\Documents\womic_driver_setup.exe /S /v"/qn /norestart "
-C:\Users\runneradmin\Documents\WOMicClientSetupV4.7.exe /S /v"/qn /norestart "
-C:\Users\runneradmin\Documents\testi.exe prevercheck /s /i hidewindow=1,confirm_d=0
+"C:\Users\runneradmin\Documents\New folder 1\vlc-3.0.16-win64.exe" /S
+"C:\Users\runneradmin\Documents\New folder 1\npp.8.1.2.Installer.x64.exe" /S
+"C:\Users\runneradmin\Documents\WPSOffice_11.2.0.10258.exe" /S
+"C:\Users\runneradmin\Documents\womic_driver_setup.exe" /S /v"/qn /norestart "
+"C:\Users\runneradmin\Documents\WOMicClientSetupV4.7.exe" /S /v"/qn /norestart "
+"C:\Users\runneradmin\Documents\testi.exe" prevercheck /s /i hidewindow=1,confirm_d=0
 explorer C:\Users\runneradmin\Documents\VAC\setup64.exe
 C:\Users\runneradmin\Documents\icons.exe /SP- /VERYSILENT
