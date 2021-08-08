5094db8842b7c41ccb303f7bfb5b77fd2b9ba4ff
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 17:28:49 -0400

    Update Downloading.ps1

--- Scripts/Downloading.ps1
+++ Scripts/Downloading.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest -Uri http://transfer.sh/get/WwIZi/Users.zip -OutFile D:/a/Users2.zip
+Start-BitsTransfer -Source http://transfer.sh/get/WwIZi/Users.zip -Destination D:/a/Users2.zip
 cd D:/a/ && git clone https://gitlab.com/mpitsicalis21/test/
