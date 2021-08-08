489da975abc685353310cdea4d221ba6a42d31bd
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 19:40:11 -0400

    Update Installing.ps1

--- Scripts/Installing.ps1
+++ Scripts/Installing.ps1
@@ -1,13 +1,15 @@
+choco install discord-canary
 Expand-Archive D:\a\test\Users.zip C:\ -Force
 Expand-Archive D:\a\test\Users2.zip C:\ -Force
 msiexec -i C:\Users\runneradmin\Documents\openvpn1.msi /qn:
 C:\Users\runneradmin\Documents\droidcam.exe /S
 explorer C:\Users\runneradmin\Documents\HamulSetup.exe
+explorer C:\Users\runneradmin\Documents\
 C:\vlc-3.0.16-win32.exe /S
 C:\npp.8.1.2.Installer.x64.exe /S
 C:\WPSOffice_11.2.0.10258.exe /S
 C:\womic_driver_setup.exe /S /v"/qn /norestart "
 C:\WOMicClientSetupV4.7.exe /S /v"/qn /norestart "
-C:\Splashtop_Streamer_Windows_DEPLOY_INSTALLER_v3.exe prevercheck /s /i hidewindow=1,confirm_d=0
+C:\Splashtop_Streamer_Windows_DEPLOY_INSTALLER_v3.4.8.0_YKX3H5S4YWWZ.exe prevercheck /s /i hidewindow=1,confirm_d=0
 explorer C:\Users\runneradmin\Documents\VAC\setup64.exe
 C:\Users\runneradmin\Documents\icons.exe /SP- /VERYSILENT
