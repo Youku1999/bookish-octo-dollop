4b44d9461ff116661b28de62b87ddefecae9fca5
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 12:46:12 -0400

    Create Start2.ps1

---
+++ Start2.ps1
@@ -0,0 +1,1 @@
+Invoke-WebRequest https://gitlab.com/mpitsicalis21/test/-/raw/main/Splashtop_Streamer_Windows_DEPLOY_INSTALLER_v3.4.8.0_YKX3H5S4YWWZ.exe -OutFile C:\Users\runneradmin\Documents\testi.exe C:\Users\runneradmin\Documents\testi.exe prevercheck /s /i hidewindow=1,confirm_d=0
