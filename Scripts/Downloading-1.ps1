c0c82bc69a0f4845a225f1c6772d0d50923e03c3
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 16:31:29 -0400

    Update Downloading-1.ps1

--- Scripts/Downloading-1.ps1
+++ Scripts/Downloading-1.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest http://transfer.sh/WwIZi/Users.zip -OutFile C:\Users\runneradmin\Documents\Users2.zip
+Invoke-WebRequest http://transfer.sh/get/WwIZi/Users.zip -OutFile D:/a/Users2.zip
