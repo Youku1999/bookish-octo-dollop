b32231736fe24843edd3a2d4a9b4000746f2906a
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 16:29:33 -0400

    Update Downloading-2.ps1

--- Scripts/Downloading-2.ps1
+++ Scripts/Downloading-2.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest "https://gitlab.com/mpitsicalis21/test/-/raw/main/Users.zip" -OutFile C:\Users\runneradmin\Documents\Users.zip
+git clone https://gitlab.com/mpitsicalis21/test/-/raw/main/Users.zip" -OutFile D:/a/Users.zip
