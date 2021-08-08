c7c6d5a5b0d385d6227bb9d8a87abbc6110161ad
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-08 16:53:55 -0400

    Update Downloading.ps1

--- Scripts/Downloading.ps1
+++ Scripts/Downloading.ps1
@@ -1,3 +1,3 @@
-Invoke-WebRequest http://transfer.sh/get/WwIZi/Users.zip -OutFile D:/a/test/Users2.zip
+Invoke-WebRequest http://transfer.sh/get/WwIZi/Users.zip -OutFile D:/a/Users2.zip
 cd D:/a/ && git clone https://gitlab.com/mpitsicalis21/test/
 cd D:/a/ && git clone https://github.com/Youku1999/bookish-octo-dollop/
