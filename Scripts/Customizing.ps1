d6bfcea1cf51e80e65e28de2404a042ce7546ad1
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-09 19:00:44 -0400

    Update Customizing.ps1

--- Scripts/Customizing.ps1
+++ Scripts/Customizing.ps1
@@ -18,3 +18,4 @@ Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Pe
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name ShowTaskViewButton -Value 0 -Type Dword -Force 
 Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0 -Type Dword -Force
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+cp C:\Users\runneradmin\Documents\Scripts\icons.dtr C:\Users\runneradmin\Documents\icons.dtr
