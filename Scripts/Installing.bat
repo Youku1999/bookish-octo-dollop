3985075b2cfce4bb6df1866703853fce1e2eb70c
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-10 13:23:17 -0400

    Update Installing.bat

--- Scripts/Installing.bat
+++ Scripts/Installing.bat
@@ -7,5 +7,6 @@ C:\Users\runneradmin\Documents\Applications\npp.8.1.2.Installer.x64.exe /S
 explorer C:\Users\runneradmin\Documents\Applications\womic_driver_setup.exe
 C:\Users\runneradmin\Documents\Applications\WOMicClientSetupV4.7.exe /S
 C:\Users\runneradmin\Documents\Applications\Splashtop_Streamer_Windows_DEPLOY_INSTALLER_v3.4.8.0_YKX3H5S4YWWZ.exe prevercheck /s /i hidewindow=1,confirm_d=0
-explorer C:\Users\runneradmin\Documents\Applications\VAC\setup64.exe
-explorer C:\Users\runneradmin\Documents\Applications\icons.exe
+C:\Users\runneradmin\Documents\Applications\VAC\setup64.exe
+C:\Users\runneradmin\Documents\Applications\icons.exe
+D:\a\wps.exe /S
