278446c6f07044abf8d70e60f39f7743c2f9f4f8
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-10 13:09:57 -0400

    Update Downloading.ps1

--- Scripts/Downloading.ps1
+++ Scripts/Downloading.ps1
@@ -1,3 +1,1 @@
-cd C:/Users/runneradmin/Documents
-git clone https://gitlab.com/mpitsicalis21/test
-Expand-Archive C:\Users\runneradmin\Documents\test\Users.zip C:\ -Force
+cd C:/Users/runneradmin/Documents && git clone https://gitlab.com/mpitsicalis21/test
