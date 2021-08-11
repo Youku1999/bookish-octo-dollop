4e858962dd1e79c8bd9a14808d750b9d25458ba2
Youku1999 <88616322+Youku1999@users.noreply.github.com>
2021-08-11 09:04:24 -0400

    Update Downloading.ps1

--- Scripts/Downloading.ps1
+++ Scripts/Downloading.ps1
@@ -1,1 +1,4 @@
-cd C:/Users/runneradmin/Documents && Invoke-WebRequest -Uri https://gitlab.com/mpitsicalis21/test/-/raw/main/Users.zip -OutFile C:\Users\runneradmin\Documents\test\Users.zip && Expand-Archive C:\Users\runneradmin\Documents\test\Users.zip C:\ -Force
+cd C:/Users/runneradmin/Documents
+mkdir C:\Users\runneradmin\Documents\test\
+Invoke-WebRequest -Uri https://gitlab.com/mpitsicalis21/test/-/raw/main/Users.zip -OutFile C:\Users\runneradmin\Documents\test\Users.zip
+Expand-Archive C:\Users\runneradmin\Documents\test\Users.zip C:\ -Force
